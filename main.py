7bdf213392233adae045a33001db960a4f17d909
commanderaipa <gundamgundam777@gmail.com>
2018-06-01 10:47:34 +0900

    [modify] まだ最中なんだけど、Q-learningでは組み合わせが多すぎるのでやめた.

--- main.py
+++ main.py
@@ -4,40 +4,95 @@ from selenium.webdriver.common.by import By
 import numpy as np
 
 
-def game_start(driver):
-    start_button = driver.find_element_by_id('button')
-    start_button.click()
+class QLearning():
+    def __init__(self, shape, gamma=0.99):
+        self.q_table = np.zeros(shape)
+        self.gamma = 
+
+    def update(self):
+        pass
 
 
-def game_over(driver):
-    ids = driver.find_elements(By.ID, 'game-over')
-    if len(ids) > 0:
-        return True
+class Agent():
+    def __init__(self, eps=1e-2):
+        # [state, action]
+        self.model = QLearning(shape=[20, 10, 4])
+        self.eps = eps
 
-    return False
+    def action(self, next_state):
+        if self.eps < np.random.uniform(0, 1):
+            next_action = np.argmax(self.model.q_table[next_state])
+        else:
+            next_action = np.random.randint(0, 4)
+
+        return next_action
+
+    def update(self):
+        self.model.update()
 
 
-def main():
-    options = ChromeOptions()
-    # ヘッドレスモードを有効にする（次の行をコメントアウトすると画面が表示される）。
-    # options.add_argument('--headless')
-    # ChromeのWebDriverオブジェクトを作成する。
-    driver = Chrome(options=options)
-    
-    # Googleのトップ画面を開く。
-    driver.get('http://localhost:1234/')
+class Game():
+    def __init__(self):
+        options = ChromeOptions()
+        # ヘッドレスモードを有効にする（次の行をコメントアウトすると画面が表示される）。
+        # options.add_argument('--headless')
+        # ChromeのWebDriverオブジェクトを作成する。
+        self.driver = Chrome(options=options)
+        
+        # Googleのトップ画面を開く。
+        self.driver.get('http://localhost:1234/')
+        self.actions = [Keys.LEFT, Keys.RIGHT, Keys.UP, Keys.DOWN]
 
-    # game start
-    game_start(driver)
+    def reset(self):
+        self.game_start()
+        return np.zeros([20, 10]), 0
 
+    def step(self, action):
+        a = self.actions[action]
+        driver.find_element_by_tag_name('body').send_keys(a)
+        
+        states = np.zeros([20, 10])
+        reward = 0
+
+        return states, reward
+
+    def game_start(self):
+        start_button = self.driver.find_element_by_id('button')
+        start_button.click()
+
+    def game_over(self):
+        ids = self.driver.find_elements(By.ID, 'game-over')
+        if len(ids) > 0:
+            return True
+
+      return False
+
+
+def main(episode=10):
+    game = Game()
+    agent = Agents()
+
+    state, reward = game.reset()
+
+    # gameの進行
     try:
-        # action
-        while True:
-            action = np.random.choice([Keys.LEFT, Keys.RIGHT, Keys.UP, Keys.DOWN])
-            driver.find_element_by_tag_name('body').send_keys(action)
+        for e in range(episode):
+            print('start episode: {}'.format(e))
+            rewards = 0
+            # action
+            while True:
+                action = agent.action(state)
+                next_state, reward = game.step(action)
+                rewards += reward
+                
+                if game.game_over():
+                    print('end episode: {}, reward: {}'.format(e, rewards))
+                    break
 
-            if game_over(driver):
-                game_start(driver)
+                # update
+                agents.update()
+                state = next_state
+
 
     except KeyboardInterrupt:
         driver.quit()  # ブラウザーを終了する。
